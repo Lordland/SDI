3a8b83e548513045a5e5d26ec5aa7c4006f9f1a8
Carlos <onecarlostlol@gmail.com>
2016-02-27 20:15:56 +0100

    Update ListarViajesAction.java

--- sesion4.MVCCasero/src/uo/sdi/acciones/ListarViajesAction.java
+++ sesion4.MVCCasero/src/uo/sdi/acciones/ListarViajesAction.java
@@ -19,32 +19,57 @@ public class ListarViajesAction implements Accion {
 		List<Trip> viajes;
 
 		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 			Map<String, String[]> mapa = request.getParameterMap();
+			Date date;
+			Trip viaje;
 			String filtro = mapa.get("filtro")[0];
-			String buscar = mapa.get("filtro")[0];
+			String buscar = mapa.get("filtroBuscar")[0];
 			if (filtro.equals("todo")) {
 				viajes = PersistenceFactory.newTripDao().findAll();
 				request.setAttribute("listaViajes", viajes);
 				Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
 						viajes.size());
+				System.out.println("huehuehuehue");
 			}
 			if(filtro.equals("fsalida")){
-				//viajes = PersistenceFactory.newTripDao().findByDepartureDate(buscar);
-				//request.setAttribute("listaViajes", viajes);
-				/*Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
-						viajes.size());*/
+				List<Trip> v = new ArrayList();
+				date = formatter.parse(buscar);
+				viajes = PersistenceFactory.newTripDao().findAll();
+				for(Trip t : viajes){
+					if(t.getDepartureDate().equals(date)){
+						v.add(t);
+					}
+				}
+				//viaje = PersistenceFactory.newTripDao().findByDepartureDate(date);
+				request.setAttribute("listaViajes", viajes);
+				Log.debug("Obtenida lista de viajes conteniendo 1 viaje");
 			}
 			if(filtro.equals("fllegada")){
-				//viajes = PersistenceFactory.newTripDao().findByDepartureDate(buscar);
-				//request.setAttribute("listaViajes", viajes);
-				/*Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
-						viajes.size());*/
+				List<Trip> v = new ArrayList();
+				date = formatter.parse(buscar);
+				viajes = PersistenceFactory.newTripDao().findAll();
+				for(Trip t : viajes){
+					if(t.getArrivalDate().equals(date)){
+						v.add(t);
+					}
+				} 
+				//viaje = PersistenceFactory.newTripDao().findByArrivalDate(date);
+				request.setAttribute("listaViajes", v);
+				Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
+						v.size());
 			}
 			if(filtro.equals("destino")){
-				//viajes = PersistenceFactory.newTripDao().findByDepartureDate(buscar);
-				//request.setAttribute("listaViajes", viajes);
-				/*Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
-						viajes.size());*/
+				List<Trip> v = new ArrayList();;
+				viajes = PersistenceFactory.newTripDao().findAll();
+				for(Trip t : viajes){
+					if(t.getDestination().getCity().equals(buscar)){
+						v.add(t);
+					}
+				}
+				request.setAttribute("listaViajes", v);
+				Log.debug("Obtenida lista de viajes conteniendo [%d] viajes",
+						v.size());
 			}
 		} catch (Exception e) {
 			Log.error("Algo ha ocurrido obteniendo lista de viajes");
